a62b31022c86f460550586a4e3494947122e5a9e
Hibernater <juliusantonaquino.joules@gmail.com>
2024-05-09 14:42:32 +0800

    SAD

--- DATSTRUC_FINALPROJ/src/Main.java
+++ DATSTRUC_FINALPROJ/src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    Main() {
+    }
+
     public static void main(String[] args) {
-        System.out.println("TITASDASDE");
+        System.out.println("Hello nigga");
+        new Main();
     }
 }
